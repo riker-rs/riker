049b5f946c92b160619906bfdc166fc6c60dc4b5
hardliner66 <hardliner66@gmail.com>
2020-07-02 18:18:40 +0200

    fix generics in macro fix tests add riker-macros to workspace use riker types in riker-macros tests

--- riker-macros/src/lib.rs
+++ riker-macros/src/lib.rs
@@ -35,8 +35,6 @@ impl MsgTypes {
 
 impl Parse for MsgTypes {
     fn parse(input: ParseStream) -> Result<Self> {
-        // caused "missing extern crate token" compile error
-        // let vars = Punctuated::<Ident, Token![,]>::parse_terminated(input)?;
         let vars = Punctuated::<Ident, syn::token::Comma>::parse_terminated(input)?;
 
         Ok(MsgTypes {
@@ -75,7 +73,7 @@ pub fn actor(
 
     let menum = types.enum_stream(&name);
     let intos = intos(&name, &types);
-    let rec = receive(&ast.ident, &ast.generics, &name);
+    let rec = receive(&ast.ident, &ast.generics, &name, &types);
 
     let input: TokenStream = input.into();
     let gen = quote! {
@@ -99,18 +97,27 @@ fn intos(name: &Ident, types: &MsgTypes) -> TokenStream {
     }
 }
 
-fn receive(aname: &Ident, gen: &Generics, name: &Ident) -> TokenStream {
+fn receive(aname: &Ident, gen: &Generics, name: &Ident, types: &MsgTypes) -> TokenStream {
     let (impl_generics, ty_generics, where_clause) = gen.split_for_impl();
+
+    let vars = types.types.iter().map(|t| {
+        let vname = &t.name;
+        let tname = &t.mtype;
+        quote! {
+            #name::#vname(msg) => <#aname #ty_generics as Receive<#tname>>::receive(self, ctx, msg, sender),
+        }
+    });
 
     quote! {
         impl #impl_generics Receive<#name> for #aname #ty_generics #where_clause {
             type Msg = #name;
-
             fn receive(&mut self,
                         ctx: &Context<Self::Msg>,
-                        msg: #name,
+                        msg: Self::Msg,
                         sender: Option<BasicActorRef>) {
-                <#aname #ty_generics>::receive(self, ctx, msg, sender);
+                match msg {
+                    #(#vars)*
+                }
             }
         }
     }
