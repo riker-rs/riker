6dbe138b80cb07b5a04d92a47646b6b99e6aee62
elenaf9 <elena.frank@protonmail.com>
2021-01-08 21:17:15 +0100

    test generic actor message

--- riker-macros/tests/macro.rs
+++ riker-macros/tests/macro.rs
@@ -95,3 +95,56 @@ fn run_derived_generic_actor() {
         std::thread::sleep(std::time::Duration::from_millis(50));
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct Message<T> {
+    inner: T,
+}
+
+#[actor(Message<String>)]
+#[derive(Clone, Default)]
+struct GenericMsgActor;
+
+impl Actor for GenericMsgActor {
+    type Msg = GenericMsgActorMsg;
+
+    fn supervisor_strategy(&self) -> Strategy {
+        Strategy::Stop
+    }
+
+    fn recv(&mut self, ctx: &Context<Self::Msg>, msg: Self::Msg, sender: Sender) {
+        self.receive(ctx, msg, sender);
+        ctx.stop(&ctx.myself);
+    }
+}
+
+impl Receive<Message<String>> for GenericMsgActor {
+    type Msg = GenericMsgActorMsg;
+
+    fn receive(
+        &mut self,
+        _ctx: &Context<Self::Msg>,
+        msg: Message<String>,
+        _sender: Option<BasicActorRef>,
+    ) {
+        println!("{}", msg.inner);
+    }
+}
+
+#[test]
+fn run_generic_message_actor() {
+    let sys = ActorSystem::new().unwrap();
+
+    let act = sys.actor_of::<GenericMsgActor>("act").unwrap();
+
+    let msg = GenericMsgActorMsg::Message(Message {
+        inner: "test".to_string(),
+    });
+    act.tell(msg, None);
+
+    // wait until all direct children of the user root are terminated
+    while sys.user_root().has_children() {
+        // in order to lower cpu usage, sleep here
+        std::thread::sleep(std::time::Duration::from_millis(50));
+    }
+}
